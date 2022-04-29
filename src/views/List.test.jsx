2c2dcc0a7d6f74fb00d8943eec389b070db52dab
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-04-29 11:33:33 -0700

    set up mock server, test is still passing. Verified test is hitting mock server and not real.

--- src/views/List.test.jsx
+++ src/views/List.test.jsx
@@ -1,5 +1,21 @@
 import { render, screen } from '@testing-library/react';
+import { setupServer }from 'msw/node';
+import { rest } from 'msw';
 import List from './List';
+
+const apiData = { 
+    data:[
+        {_id:"5e93b4a43af44260882e33b0",firstname:"Michael",lastname:"Scott",__v:0},
+        {_id:"5e93b4f03af44260882e33b1",firstname:"Jim",lastname:"Halpert",__v:0},
+  ]};
+
+  const server = setupServer(
+    rest.get('https://www.officeapi.dev/api/characters/', (req, res, ctx) => res(ctx.json(apiData)))
+  );
+
+  beforeAll(() => server.listen());
+    afterEach(() => server.resetHandlers());
+    afterAll(() => server.close);
 
 describe('List', () => {
 
